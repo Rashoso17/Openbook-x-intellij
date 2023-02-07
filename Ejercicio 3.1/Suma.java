d1550b5f4acc70faf321883cb039ea6f236219c9
Rashoso <Rashosoaov@gmail.com>
2023-02-07 16:09:06 +0100

    Suma

---
+++ Ejercicio 3.1/Suma.java
@@ -0,0 +1,9 @@
+public class Suma {
+    public static void main(String[] args) {
+        int resultado;
+        resultado = suma( 4, 2, 3);
+        System.out.println(resultado);
+    }
+    public static int suma(int a, int b, int c) {return a + b + c;
+    }
+}
